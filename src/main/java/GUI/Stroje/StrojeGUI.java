718ac6abb5996443bd706554b9289342618e7fcf
Mario <stachera.mario100@gmail.com>
2024-03-13 15:27:53 +0100

    4.5 - drobné upravy

--- src/main/java/GUI/Stroje/StrojeGUI.java
+++ src/main/java/GUI/Stroje/StrojeGUI.java
@@ -168,6 +168,8 @@ public class StrojeGUI extends javax.swing.JFrame implements BTypStrojaGUIClosed
 
         jLabel5.setText("vyradenie:");
 
+
+
         jLabel6.setText("popis:");
 
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
